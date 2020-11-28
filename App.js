272c632d648a568f4e4548fccba774512d91afde
shariq-90 <Shariqahmed80@hotmail.com>
2020-11-28 23:46:03 +0500

    task 3

--- App.js
+++ App.js
@@ -5,71 +5,27 @@ import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-nativ
 export default function App() {
   const [original_price,set_original_price]=useState();
   const [discount_percentage,set_discount_percentage]=useState();
+  const [save_price,set_save_price]=useState();
+  const [final_price,set_final_price]=useState();
 
 
-  return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Discount Calculator</Text>
-        <Text style={styles.text}>Enter your original price:</Text>
-        <TextInput placeholder="Original Price" style={styles.input}></TextInput>
-        <Text>Enter your discount percentage:</Text>
-        <TextInput placeholder="Discount %" style={styles.input}></TextInput>
-        <TouchableOpacity style={styles.button}>Calculate</TouchableOpacity>
-      </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    alignContent:"center",
-  },
-  header: {
-    textAlign: "center",
-    backgroundColor: "grey",
-    padding: 5,
-    fontSize: 30,
-    fontWeight: "bold",
-    fontFamily:"Sanserif",
-    marginBottom:50,
-    color:"white",
-    shadowOpacity:2,
-
-  },
-  input: {
-    marginTop:5,
-    borderWidth: 2,
-    width: "50%",
-    margin:"auto",
-    height:40,
-    borderRadius: 12,
-    borderColor: "silver",
-  },
-  button: {
-    padding:10,
-    textAlign:"center",
-    borderRadius:10,
-    marginTop:15,
-    margin:"auto",
-    backgroundColor:"grey",
-    width:"50%",
+  const resultHandler = () => {
+    set_save_price(original_price*(discount_percentage/100).toFixed(2))
+    set_final_price(original_price - original_price*(discount_percentage/100).toFixed(2))
   }
-});import React, {useState} from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-
-
-export default function App() {
-  const [original_price,set_original_price]=useState();
-  const [discount_percentage,set_discount_percentage]=useState();
-
-
   return (
       <View style={styles.container}>
         <Text style={styles.header}>Discount Calculator</Text>
         <Text style={styles.text}>Enter your original price:</Text>
-        <TextInput placeholder="Original Price" style={styles.input}></TextInput>
+        <TextInput placeholder="Original Price" style={styles.input}
+                   onChange={(e) => set_original_price(e.target.value)}></TextInput>
         <Text>Enter your discount percentage:</Text>
-        <TextInput placeholder="Discount %" style={styles.input}></TextInput>
-        <TouchableOpacity style={styles.button}>Calculate</TouchableOpacity>
+        <TextInput placeholder="Discount %" style={styles.input}
+                   onChange={(e) => set_discount_percentage(e.target.value)}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={resultHandler}>Calculate</TouchableOpacity>
+        <Text style={styles.result}>You Saved:{save_price}</Text>
+        <Text style={styles.result}>Final Price:{final_price}</Text>
       </View>
   );
 }
@@ -89,6 +45,7 @@ const styles = StyleSheet.create({
     color:"white",
     shadowOpacity:2,
 
+
   },
   input: {
     marginTop:5,
@@ -98,6 +55,8 @@ const styles = StyleSheet.create({
     height:40,
     borderRadius: 12,
     borderColor: "silver",
+    fontFamily:"Sanserif",
+
   },
   button: {
     padding:10,
@@ -107,5 +66,18 @@ const styles = StyleSheet.create({
     margin:"auto",
     backgroundColor:"grey",
     width:"50%",
+    marginBottom:5,
+    fontFamily:"Sanserif",
+    shadowOpacity:2,
+  },
+  result:{
+    margin:"auto",
+    padding:5,
+    marginBottom:5,
+    backgroundColor:"grey",
+    fontSize:20,
+    fontFamily:"Sanserif",
+    fontWeight:"bold",
+
   }
 });
